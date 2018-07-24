8a525d015bac636202e6154a6ac6967f5d7851b0
instructor <groyce@unomaha.edu>
2018-07-23 20:06:42 -0500

    changes

--- efs/settings.py
+++ efs/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-LOGIN_REDIRECT_URL = '/home'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
